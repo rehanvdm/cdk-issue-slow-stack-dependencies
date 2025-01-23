8cbc55ecff138ed55069b43c35ed8c7464c87d8e
rehanvdm <rehan.vdm4@gmail.com>
2025-01-23 13:56:16 +0200

    feat: add the debug/fix code mentioned in the issue

--- bin/cdk-issue-slow-stack-dependencies.ts
+++ bin/cdk-issue-slow-stack-dependencies.ts
@@ -1,16 +1,35 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import {App,Stack,} from "aws-cdk-lib";
+import {App, Names, Stack,} from "aws-cdk-lib";
 
 const app = new App();
-const numberOfWaves = 9;
+const numberOfWaves = 10;
 const numberOfStacks = 10;
+
+class DebugStack extends Stack {
+  constructor(scope: App, id: string) {
+    super(scope, id);
+  }
+
+  addDependency(target: Stack, reason?: string) {
+    (this as any)._stackDependencies[Names.uniqueId(target)] = {
+      stack: target,
+      reasons: [
+        {
+          source: this,
+          target: target,
+          reason: reason,
+        },
+      ],
+    };
+  }
+}
 
 function createWaveStacks(app: App, waveNumber: number, numberOfStacks: number,
                           previousWaveStacks: Stack[] = []): Stack[] {
   const waveStacks: Stack[] = [];
   for (let i = 0; i < numberOfStacks; i++) {
-    const stack = new Stack(app, `CdkWave${waveNumber}-${i}`);
+    const stack = new DebugStack(app, `CdkWave${waveNumber}-${i}`);
     waveStacks.push(stack);
 
     for (const previousStack of previousWaveStacks) {
@@ -21,8 +40,10 @@ function createWaveStacks(app: App, waveNumber: number, numberOfStacks: number,
   return waveStacks;
 }
 
+// console.log("HEAP BEFORE", process.memoryUsage());
 let previousWaveStacks: Stack[] = [];
 for (let waveNumber = 1; waveNumber <= numberOfWaves; waveNumber++) {
   previousWaveStacks = createWaveStacks(app, waveNumber, numberOfStacks, previousWaveStacks);
 }
 app.synth();
+// console.log("HEAP AFTER", process.memoryUsage());
